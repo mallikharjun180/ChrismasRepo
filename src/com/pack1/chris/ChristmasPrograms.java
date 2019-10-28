d4cc52007ba7f1bd9673832c5b0de5619b724f13
mallikharjun <mallikharjun180@gmail.com>
2019-10-28 14:46:09 +0530

    ChristmasClelebration

---
+++ src/com/pack1/chris/ChristmasPrograms.java
@@ -0,0 +1,28 @@
+package com.pack1.chris;
+
+import org.testng.annotations.Test;
+
+public class ChristmasPrograms 
+{
+
+	@Test
+	public void  christmasM1(){
+		System.out.println("christmas celebration ");
+	}
+	@Test
+	public void christmasM2(){
+		System.out.println("christmas celebration start ");
+	}
+	@Test
+	public void christmasM3(){
+		System.out.println("christmas celebration continue ");
+	}
+	@Test
+	public void christmasM4(){
+		System.out.println("christmas celebration done ");
+	}
+	@Test
+	public void christmasM5(){
+		System.out.println("christmas celebration over ");
+	}
+}
