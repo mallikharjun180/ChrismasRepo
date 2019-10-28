44ad18e7a9ad074e49710a6cace9ff377ed2fe85
mallikharjun <mallikharjun180@gmail.com>
2019-10-28 14:06:53 +0530

    ChristmasBuild

---
+++ src/christmasPackage/ChristmasClass.java
@@ -0,0 +1,24 @@
+package christmasPackage;
+
+import org.testng.annotations.Test;
+
+public class ChristmasClass
+{
+
+	@Test
+	public void m1(){
+		System.out.println("divali celebration ");
+	}
+	@Test
+	public void m2(){
+		System.out.println("divali celebration start ");
+	}
+	@Test
+	public void m3(){
+		System.out.println("divali celebration continue ");
+	}
+	@Test
+	public void m4(){
+		System.out.println("divali celebration done ");
+	}
+}
